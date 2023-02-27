3ff2155aabc69cc3016e5a79021e51e841ccdc99
test <testemail@test.com>
2023-02-27 13:30:19 +0100

    I add a new package

---
+++ src/Day2/JavaCode.java
@@ -0,0 +1,9 @@
+package Day2;
+
+public class JavaCode {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author ma_zu
- *
- */
-module practiceWithSalmaApu {
-}
